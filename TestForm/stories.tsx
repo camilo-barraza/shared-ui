589bea7442d7be6d3dfbd2a6cc15d9ec57a6d2ca
Camilo Barraza <cbarraza11personal@gmail.com>
2022-03-18 20:39:07 -0500

    add Test form component

---
+++ TestForm/stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TestForm } from ".";
+
+export default {
+  component: TestForm,
+  title: "TestForm",
+};
+
+export const Base = () => {
+  return <TestForm />;
+};
